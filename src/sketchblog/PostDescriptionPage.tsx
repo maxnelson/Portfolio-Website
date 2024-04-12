40be8c8bda79cf1eec88f652b468be250006c8e1
Max Nelson <maxnelson@MaxBook2.local>
2024-04-12 01:57:12 -0700

    fixing up sketchblog

--- src/sketchblog/PostDescriptionPage.tsx
+++ src/sketchblog/PostDescriptionPage.tsx
@@ -3,6 +3,7 @@ import { getPost } from "@/sketchblog/GetPostCategory";
 import { useState, useEffect } from "react";
 import { SketchblogSidebar } from "@/sketchblog/SketchblogSidebar";
 import { DocumentData } from "@firebase/firestore";
+import { SketchblogFooter } from "@/sketchblog/SketchblogFooter";
 
 /*
 type postObjectType = {
@@ -38,45 +39,42 @@ export function PostDescriptionPage() {
         <>
           <div className="sketchblog_page_background"></div>
           <div className="sketchblog_detail_page_container">
-            <div className="sketchblog_post_detail_container">
-              <div>
-                <div
-                  className={
-                    "sketchblog_post_detail_images_container" +
-                    "post_detail_images_container_" +
-                    postObject.format
-                  }
-                >
-                  {postObject.images.map((image, index) => {
-                    return (
-                      <div className="post_image_container" key={index}>
-                        <img src={image} alt="drawing" />
-                      </div>
-                    );
-                  })}
-                  <hr className="sketchblog_post_hr" />
-                  <div className="post_text_container">
-                    <p className="post_date">
-                      {postObject.date.toDate().toLocaleDateString("en-US", {
-                        month: "short",
-                        day: "2-digit",
-                        year: "numeric",
-                      })}
-                    </p>
-                  </div>
-                </div>
-
-                <div className="sketchblog_post_detail_description_container">
-                  <div>
-                    <h2>{postObject.title}</h2>
-                  </div>
-                  <div
-                    dangerouslySetInnerHTML={{ __html: postObject.description }}
-                  />
+            <div className="width-85-percent">
+              <div
+                className={
+                  "sketchblog_post_detail_images_container " +
+                  "post_detail_images_container_" +
+                  postObject.format
+                }
+              >
+                {postObject.images.map((image, index) => {
+                  return (
+                    <div className="post_image_container" key={index}>
+                      <img src={image} alt="drawing" />
+                    </div>
+                  );
+                })}
+                <hr className="sketchblog_post_hr" />
+                <div className="post_text_container">
+                  <p className="post_date">
+                    {postObject.date.toDate().toLocaleDateString("en-US", {
+                      month: "short",
+                      day: "2-digit",
+                      year: "numeric",
+                    })}
+                  </p>
                 </div>
               </div>
-              <SketchblogSidebar />
+
+              <div className="sketchblog_post_detail_description_container">
+                <h2>{postObject.title}</h2>
+                <div
+                  dangerouslySetInnerHTML={{ __html: postObject.description }}
+                />
+              </div>
             </div>
+            <SketchblogSidebar />
+            <SketchblogFooter />
           </div>
         </>
       )}
