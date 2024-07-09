e8a623fa5374d7c5828d8c554d80410f6f105767
maxnelson <maxnelson88@gmail.com>
2024-07-09 01:48:26 -0700

    fixed up resume formatting

--- src/components/Resume/SummarySection.tsx
+++ src/components/Resume/SummarySection.tsx
@@ -3,7 +3,7 @@ import { resumeStyles as styles } from "@/components/Resume/resumeStyles";
 export const SummarySection = () => {
   return (
     <View>
-      <Text style={[styles.margin_top_5, styles.font_size_medium]}>
+      <Text style={[styles.font_size_medium]}>
         Web Developer with eight years of experience developing web applications
         using a variety of languages and frameworks, and website components
         using Javascript, HTML, and CSS.
