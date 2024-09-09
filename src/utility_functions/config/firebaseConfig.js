ac5f8bc972e66a6eb2beea8e1253686367a7d160
maxnelson <maxnelson88@gmail.com>
2024-09-08 21:49:32 -0700

    updated dependancies

--- src/utility_functions/config/firebaseConfig.js
+++ src/utility_functions/config/firebaseConfig.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: "AIzaSyCk8mxT5l7myXYe5lKbjuhL04aeMzaYMTg",
   authDomain: "portfolio-website-410901.firebaseapp.com",
   projectId: "portfolio-website-410901",
   storageBucket: "portfolio-website-410901.appspot.com",
   messagingSenderId: "997554075481",
-  appId: process.env.FIREBAE_APP_ID,
-  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+  appId: "1:997554075481:web:c8f57ef874ebac5b593e48",
+  measurementId: "G-9PKFHG5J4R",
 };
 
 export const firebase_app = initializeApp(firebaseConfig);
