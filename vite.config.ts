7f47c13e18763cad90dfebba741b9990d2fd5266
maxnelson <maxnelson88@gmail.com>
2024-09-26 13:02:26 -0700

    fixing up About section and other small changes

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,28 @@
 import { defineConfig } from "vite";
 import path from "path";
 import react from "@vitejs/plugin-react";
-import baristaCSS from "@modularmoon/barista";
+//import baristaCSS from "@modularmoon/barista";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    baristaCSS({
-      include: ["src/**/*.{js,ts,jsx,tsx,html}"],
-      outputFilepath: "dist/css/barista.css",
-      delimiter1: "_",
-      delimiter2: "--",
-    }),
-  ],
+  plugins: [react()],
   server: { port: 5100 },
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@server": path.resolve(__dirname, "./server"),
+      "@": path.resolve(
+        __dirname,
+        "./src"
+      ),
+      "@server": path.resolve(
+        __dirname,
+        "./server"
+      ),
+    },
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        api: "modern",
+      },
     },
   },
 });
