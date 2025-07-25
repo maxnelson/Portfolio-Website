cf3a3e7525ae356dfebeba5124511eafe6e9dbee
maxnelson <maxnelson88@gmail.com>
2025-07-25 16:55:23 -0700

    continuing performance optimizations

--- vite.config.ts
+++ vite.config.ts
@@ -3,37 +3,49 @@ import path from "path";
 import react from "@vitejs/plugin-react";
 import baristaCSS from "@modularmoon/barista";
 
-export default defineConfig({
-  plugins: [
-    react(),
-    baristaCSS({
-      include: [
-        "src/**/*.{js,ts,jsx,tsx,html}",
-      ],
-      outputFilepath:
-        "src/css/barista.css",
-      delimiter1: "_",
-      delimiter2: "--",
-    }),
-  ],
-  server: { port: 5100 },
-  resolve: {
-    alias: {
-      "@": path.resolve(
-        __dirname,
-        "./src"
-      ),
-      "@server": path.resolve(
-        __dirname,
-        "./server"
-      ),
-    },
-  },
-  css: {
-    preprocessorOptions: {
-      scss: {
-        api: "modern",
-      },
-    },
-  },
-});
+export default defineConfig(({ mode }) => ({
+	test: {},
+	base: "/",
+	plugins: [
+		react(),
+		baristaCSS({
+			include: ["src/**/*.{js,ts,jsx,tsx,html}"],
+			outputFilepath: "src/css/barista.css",
+			delimiter1: "_",
+			delimiter2: "--"
+		})
+	],
+	resolve: {
+		alias: {
+			"@": path.resolve(__dirname, "./src")
+		}
+	},
+	css: {
+		devSourcemap: true
+	},
+
+	build: {
+		outDir: "dist",
+		terserOptions: {
+			compress: {
+				drop_debugger: true
+			},
+			format: {
+				comments: false
+			}
+		}
+	},
+	server:
+		mode === "development" ?
+			{
+				port: 5100,
+				proxy: {
+					"/server": {
+						target: "http://localhost:8080",
+						changeOrigin: true,
+						secure: false
+					}
+				}
+			}
+		:	{}
+}));
